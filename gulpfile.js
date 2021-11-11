c0392183d01def173d0325f486454bb8ea4c9d77
thaind-2785 <nguyen.duy.thai@sun-asterisk.com>
2021-11-11 10:26:15 +0700

    Task.1 - Set folder structure, install tool

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest, parallel, watch, series } = require('gulp'), concat = require('gulp-concat'), 
+sass = require('gulp-sass')(require('sass')), pug = require('gulp-pug'), browserSync = 
+require('browser-sync').create()
+const FilesPath = { sassFiles: 'src/sass/*.scss', htmlFiles: 'src/pug/pages/*.pug' }
+const {sassFiles, htmlFiles} = FilesPath
+// task 1: sass -> css
+function sassTask() { return src(sassFiles) .pipe(sass()) .pipe(concat('style.css')) 
+.pipe(dest('assets/css')) .pipe(browserSync.stream()); }
+
+// task 2: pug -> html
+function htmlTask() { return src(htmlFiles) .pipe(pug({ pretty: true })) 
+.pipe(dest('./')) .pipe(browserSync.stream()); } 
+
+// task 3: tạo server với live reload
+function serve() { browserSync.init({ server: { baseDir: './' } }); watch('src/sass/**/*', 
+    sassTask); watch('src/pages/**/*', htmlTask); }
+
+// Gọi gulp bằng 1 dòng lệnh
+exports.sass = sassTask; 
+exports.html = htmlTask; 
+exports.default = series(parallel(htmlTask, sassTask)); 
+exports.serve = series(serve, parallel(htmlTask, sassTask))
